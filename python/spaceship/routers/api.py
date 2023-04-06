b235facc87810c0794ea61f23bdad3d027abe4c9
Illyakravchuk <ilakrawchyk18@gmail.com>
2023-04-06 17:50:41 +0300

    added matrix product endpoint
    Added to the Dockerfile the necessary for installing the dependencies also in the file
    requirements/backend.in added dependencies.
    In the spaceship/routers/api.py file, added a matrix product endpoint.
    Also added the result of matrix generation to the spaceship/routers/res_matrices.json file.

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,8 +1,19 @@
 from fastapi import APIRouter
+import numpy
 
 router = APIRouter()
 
 
-@router.get('')
-def hello_world() -> dict:
-    return {'msg': 'Hello, World!'}
+@router.get('/matrix')
+def matrix() -> dict:
+    matrix_a = numpy.random.rand(10, 10)
+    matrix_b = numpy.random.rand(10, 10)
+    product = numpy.dot(matrix_a, matrix_b)
+
+    result = {
+        "matrix_A": matrix_a.tolist(),
+        "matrix_B": matrix_b.tolist(),
+        "product of matrices": product.tolist()
+    }
+
+    return result
